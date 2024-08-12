da1761824bd62f7cbcc1f0efda61256d39616f8d
Dean Winchester <110895086+Winchester-Dean@users.noreply.github.com>
2024-08-12 11:32:17 +0300

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,18 @@
+
+$(function () {
+    $(".typed").typed({
+        strings: [
+            "Dean Winchester",
+            "Программист на...",
+            "Python",
+            "C++",
+            "Так же знаю",
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        typeSpeed: 50,
+        loop: true,
+        cursorChar: "|",
+    });
+});
